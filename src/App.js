a8efc07890ef2825466d7606e1ae523b829eb11b
Andres Urrea <andres.urreao@gmail.com>
2018-02-24 21:19:56 -0500

    Iteration-on-array

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css';
 
 /// Mostrar una lista en base a un arreglo
 
@@ -14,7 +15,9 @@ class App extends Component {
       <div style={styles.container}>
         <h1 style={styles.title}>Las tecnologias del Front</h1>
         <ul style={styles.list}>
-          {/* Tu codigo va aca */}
+          {this.state.list.map((item, i) =>
+            <li key={i}>{item}</li>
+          )}
         </ul>
       </div>
     );
